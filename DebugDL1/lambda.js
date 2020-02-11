ece2ac5db42d6e7aaf4c35bdc3984356df00bd41
asankhaperera <asankha.perera@gmail.com>
2020-02-11 21:46:13 +0530

    init

---
+++ DebugDL1/lambda.js
@@ -0,0 +1,23 @@
+let AWS = require('aws-sdk');
+let SL_AWS = require('slappforge-sdk-aws');
+const sqs = new SL_AWS.SQS(AWS);
+
+exports.handler = async (event) => {
+    debugger;
+
+    let messageBody = "Hello World 1";
+
+    console.log(messageBody);
+
+    try {
+        let data = await sqs.sendMessage({
+            MessageBody: messageBody,
+            QueueUrl: `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.env.SIGMA_AWS_ACC_ID}/sqs`,
+            DelaySeconds: '0',
+            MessageAttributes: {}
+        }).promise();
+        return "Submitted : " + messageBody;
+    } catch (error) {
+        return "Failed";
+    }
+};
